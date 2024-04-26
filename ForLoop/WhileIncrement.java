43ab12cd753e1e3c39e4609586891a187fff33cd
Philip  Amista <140089679+messiderata@users.noreply.github.com>
2024-04-26 13:14:35 +0800

    Update WhileIncrement.java

--- ForLoop/WhileIncrement.java
+++ ForLoop/WhileIncrement.java
@@ -1,11 +1,1 @@
-package ForLoop;
 
-public class WhileIncrement {
-    public static void main(String[] args) {
-        int count = 1;
-        while (count <= 10) {
-            System.out.println(count);
-            count++;
-        }
-    }
-}
